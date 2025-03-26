389d0a317e869caa90825f24d2f49cca1476639a
Your Name <you@example.com>
2025-03-26 09:32:14 -0300

    new try

--- list.c
+++ list.c
@@ -29,11 +29,17 @@ Node * createNode(void * data) {
 }
 
 List * createList() {
-     return NULL;
+    List* lista = (List*)malloc(sizeof(List));
+    lista->head = NULL;
+    lista->tail = NULL;
+    lista->current = NULL;
+    return lista;
 }
 
 void * firstList(List * list) {
-    return NULL;
+    if(list->head == NULL) return NULL;
+    list->current = list->head;
+    return list->current->data;
 }
 
 void * nextList(List * list) {
